f42d0b04952b1adfd68cb147090dd073a86119eb
Alexander <satan1c@ukr.net>
2022-07-30 20:27:27 +0300

    ECS systems

---
+++ Systems/General/GeneratorSystem.cs
@@ -0,0 +1,104 @@
+﻿using System.Linq;
+using Leopotam.Ecs;
+using Types.Interfaces;
+using Types.Structs;
+using UnityEngine;
+
+namespace Systems.General
+{
+    public class GeneratorSystem : IEcsPreInitSystem
+    {
+        public EcsWorld World = null;
+        public MapData MapData = null;
+
+        public void PreInit()
+        {
+            MapData.InitMap();
+            GenerateBiomes();
+        }
+
+        private void GenerateBiomes()
+        {
+            for (byte biomeIndex = 0; biomeIndex < MapData.MapSize; biomeIndex++)
+            {
+                var biomeConfig = MapData.GetBiomeConfig(biomeIndex);
+                var biomeEntity = World.NewEntity();
+
+                ref var biome = ref biomeEntity.Get<Biome>();
+                biome.Id = biomeIndex;
+                biome.InitChunks(MapData.BiomeSize);
+
+                GenerateChunks(ref biomeConfig, ref biome);
+                MapData.SetBiome(biomeIndex, biomeEntity.Ref<Biome>());
+            }
+        }
+
+        private void GenerateChunks(ref BiomeConfig biomeConfig, ref Biome biome)
+        {
+            for (uint chunkIndex = 0; chunkIndex < MapData.BiomeSize; chunkIndex++)
+            {
+                var chunkEntity = World.NewEntity();
+
+                ref var chunk = ref chunkEntity.Get<Chunk>();
+                chunk.Id = chunkIndex;
+                chunk.BiomeId = biome.Id;
+                chunk.Grounds = new EcsComponentRef<Ground>[MapData.ChunkSize];
+
+                GenerateGrounds(ref biomeConfig, ref chunk);
+                biome.Chunks[chunkIndex] = chunkEntity.Ref<Chunk>();
+            }
+        }
+
+        private void GenerateGrounds(ref BiomeConfig biomeConfig, ref Chunk chunk)
+        {
+            for (byte groundIndex = 0; groundIndex < MapData.ChunkSize; groundIndex++)
+            {
+                var groundEntity = World.NewEntity();
+                var groundConfig = GetBest(ref biomeConfig.groundConfigs);
+
+                ref var ground = ref groundEntity.Get<Ground>();
+                ground.Id = groundIndex;
+                ground.ChunkId = chunk.Id;
+                ground.BiomeId = chunk.BiomeId;
+                ground.Parts = new GroundPart[groundConfig.isPlaceable ? MapData.GroundSize : 0];
+                ground.MainTexture = groundConfig.textureVariants[Random.Range(0, groundConfig.textureVariants.Length)];
+
+                if (groundConfig.isPlaceable)
+                    GenerateParts(ref biomeConfig, ref ground);
+
+                chunk.Grounds[groundIndex] = groundEntity.Ref<Ground>();
+            }
+        }
+
+        private void GenerateParts(ref BiomeConfig biomeConfig, ref Ground ground)
+        {
+            var parts = new GroundPartConfig[MapData.GroundSize];
+            for (byte partIndex = 0; partIndex < MapData.GroundSize; partIndex++)
+            {
+                var variant = GetBest(ref biomeConfig.groundPartConfigs);
+                parts[partIndex] = variant.Chance < Random.value
+                    ? new GroundPartConfig {type = PartType.None, state = PartState.None}
+                    : variant;
+            }
+
+            if (parts.Any(x => x.type == PartType.Big || x.type == PartType.Struct))
+                parts = new[] {parts.First(x => x.type == PartType.Big || x.type == PartType.Struct)};
+            else
+                parts[0] = new GroundPartConfig {type = PartType.None, state = PartState.None};
+
+            for (byte partIndex = 0; partIndex < parts.Length; partIndex++)
+                ground.Parts[partIndex] = new GroundPart(parts[partIndex]);
+        }
+
+        public static T GetBest<T>(ref T[] variants) where T: IVariant
+        {
+            T variant;
+
+            do
+                variant = variants[Random.Range(0, variants.Length)];
+            while (variant.Chance < Random.value);
+            
+            return variant;
+        }
+    }
+}
---
+++ Systems/General/WorldStateSystem.cs
@@ -0,0 +1,214 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Leopotam.Ecs;
+using Pathfinding;
+using Types.Classes;
+using Types.Structs;
+using UnityEngine;
+using Chunk = Types.Structs.Chunk;
+using Ground = Types.Structs.Ground;
+
+namespace Systems.General
+{
+    public class WorldStateSystem : IEcsInitSystem
+    {
+        //public EcsWorld World = null;
+        public Player PlayerInstance;
+        public MapData MapData;
+
+        private byte m_currentBiomeId;
+        private uint m_currentChunkId;
+        //private byte CurrentGroundId = 0;
+
+        private uint m_pivot;
+        private byte m_chunkRowSize = 27;
+        private Vector2 m_vectors;
+
+        private readonly Chunk[] m_currentChunks = new Chunk[9];
+        private readonly uint[] m_indexes = new uint[9];
+
+        private readonly WaitForFixedUpdate m_fixedUpdateTimer = new();
+        private readonly WaitForSecondsRealtime m_waitTwoSeconds = new(2);
+
+        public void Init()
+        {
+            m_vectors = new Vector2(10, 6);
+            m_pivot = MapData.BiomeSize / 2;
+            m_chunkRowSize = (byte) Mathf.RoundToInt(Mathf.Sqrt(MapData.BiomeSize));
+            m_currentChunkId = m_pivot;
+            
+            //UpdateCluster();
+
+            //var position = (Vector2) MapData.cluster.chunks[4].transform.position;
+            //var offset = GetOffset(ref position);
+            //var indexes = GetChunkIndexes(ref offset);
+            
+            UpdateChunkIndexes(ref m_pivot);
+            UpdateCurrentChunks();
+            UpdateCluster();
+
+            MapData.StartCoroutine(StateUpdater());
+            MapData.AstarPath.Scan();
+        }
+
+        private IEnumerator StateUpdater()
+        {
+            while (true)
+            {
+                /*yield return _fixedUpdateTimer;
+                
+                if (GetClosestChunk(out var closestChunk).Id == _currentChunkId &&
+                    closestChunk.BiomeId == _currentBiomeId) continue;*/
+
+                yield return m_waitTwoSeconds;
+                
+                if (GetClosestChunk(out var closestChunk).Id == m_currentChunkId &&
+                    closestChunk.BiomeId == m_currentBiomeId) continue;
+
+                UpdateState(closestChunk);
+            }
+        }
+
+        private void UpdateState(Chunk closestChunk)
+        {
+            //TODO: кривое отображение чанков
+            m_currentBiomeId = closestChunk.BiomeId;
+            m_currentChunkId = closestChunk.Id;
+            UpdateChunkIndexes(ref m_currentChunkId);
+            UpdateCurrentChunks();
+            UpdateCluster();
+
+            ((MapData.AstarPath.graphs[0] as GridGraph)!).center = MapData.cluster.Transform.position;
+            MapData.AstarPath.Scan();
+        }
+
+        private Chunk GetClosestChunk(out Chunk chunk)
+        {
+            Chunk? result = null;
+            
+            var chunks = MapData.cluster.chunks;
+            var playerPosition = (Vector2) PlayerInstance.transform.position;
+            var lastPosition = (Vector2) chunks[4].transform.position;
+            var closestPosition = lastPosition;
+
+            {
+                var closestDistance = Vector2.Distance(lastPosition, playerPosition);
+
+                foreach (var c in chunks)
+                {
+                    var chunkPosition = (Vector2) c.transform.position;
+                    var distance = Vector2.Distance(chunkPosition, playerPosition);
+
+                    if (!(distance < closestDistance)) continue;
+
+                    closestPosition = chunkPosition;
+                    closestDistance = distance;
+                }
+            }
+            
+            if (closestPosition != lastPosition)
+            {
+                var closest = (closestPosition - lastPosition) / m_vectors;
+
+                MapData.cluster.transform.position += (Vector3) (closestPosition - lastPosition);
+                result = m_currentChunks[GetOffset(ref closest)];
+            }
+
+            result ??= m_currentChunks[4];
+            
+            chunk = (Chunk) result;
+            return chunk;
+        }
+
+        private void UpdateCurrentChunks()
+        {
+            var biome = MapData.GetBiome(m_currentBiomeId).Unref();
+
+            for (byte i = 0; i < 9; i++)
+                m_currentChunks[i] = biome.Chunks[m_indexes[i]].Unref();
+        }
+
+        private void UpdateChunkIndexes(ref uint position)
+        {
+            m_indexes[4] = position;
+            m_indexes[3] = position - 1;
+            m_indexes[5] = position + 1;
+
+            m_indexes[1] = GetOffset(0, 1, position, m_chunkRowSize);
+            m_indexes[0] = m_indexes[1] - 1;
+            m_indexes[2] = m_indexes[1] + 1;
+
+            m_indexes[7] = GetOffset(0, -1, position, m_chunkRowSize);
+            m_indexes[6] = m_indexes[7] - 1;
+            m_indexes[8] = m_indexes[7] + 1;
+        }
+
+        private static uint GetOffset(ref Vector2 position, uint pivot = 4, uint rowSize = 3) 
+            => GetOffset((int) position.x, (int) position.y, pivot, rowSize);
+        private static uint GetOffset(int x, int y, uint pivot = 4, uint rowSize = 3) 
+            => (uint) ((x + pivot) - (rowSize * y));
+
+        private void UpdateCluster()
+        {
+            //ref var biomeData = ref MapData.Map[_currentBiomeId].Unref();
+            
+            for (byte i = 0; i < MapData.cluster.chunks.Length; i++)
+            {
+                //SetChunk(ref biomeData, ref i, ref _indexes[i]);
+                var chunkInstance = MapData.cluster.chunks[i];
+                
+                for (byte j = 0; j < chunkInstance.grounds.Length; j++)
+                {
+                    SetGround(ref m_currentChunks[i], ref j, ref chunkInstance);
+                }
+            }
+        }
+
+        private void SetChunk(ref Biome biomeData, ref byte instanceIndex, ref uint chunkIndex)
+        {
+            ref var chunkData = ref biomeData.Chunks[chunkIndex].Unref();
+            var chunkInstance = MapData.cluster.chunks[instanceIndex];
+
+            for (byte j = 0; j < chunkInstance.grounds.Length; j++)
+                SetGround(ref chunkData, ref j, ref chunkInstance);
+        }
+
+        private void SetGround(ref Chunk chunkData, ref byte index, ref Types.Classes.Chunk chunkInstance)
+        {
+            ref var groundData = ref chunkData.Grounds[index].Unref();
+            var groundInstance = chunkInstance.grounds[index];
+
+            groundInstance.spriteRenderer.sprite = groundData.MainTexture;
+            
+            if (groundData.Parts.Length > 0)
+            {
+                SetParts(ref groundData, ref groundInstance);
+                return;
+            }
+
+            for (byte partIndex = 0; partIndex < MapData.groundSize; partIndex++)
+            {
+                var part = groundInstance.parts[partIndex];
+                part.spriteRenderer.sprite = null;
+                part.collectCollider.enabled = false;
+                part.collisionCollider.enabled = false;
+            }
+        }
+
+        private static void SetParts(ref Ground groundData, ref Types.Classes.Ground groundInstance)
+        {
+            for (byte partIndex = 0; partIndex < groundData.Parts.Length; partIndex++)
+            {
+                var partInstance = groundInstance.parts[partIndex];
+                var partData = groundData.Parts[partIndex];
+
+                partInstance.spriteRenderer.sprite = partData.MainTexture;
+                partInstance.collectCollider.enabled = partData.HasCollector;
+                partInstance.collisionCollider.enabled = partData.HasCollision;
+
+                if (partData.HasCollision)
+                    partInstance.gameObject.layer = 8;
+            }
+        }
+    }
+}
