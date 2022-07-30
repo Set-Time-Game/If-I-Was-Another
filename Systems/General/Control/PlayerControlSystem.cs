80158b513c5d79b19c3d7246e82f5a488952e7c4
Alexander <satan1c@ukr.net>
2022-07-30 20:29:55 +0300

    ECS systems

---
+++ Systems/General/Control/PlayerControlSystem.cs
@@ -0,0 +1,24 @@
+﻿using Leopotam.Ecs;
+using Types.Classes;
+using Chunk = Types.Structs.Chunk;
+
+namespace Systems.General.Control
+{
+    public class PlayerControlSystem : IEcsRunSystem, IEcsPreInitSystem
+    {
+        public MapData MapData = null;
+        public Player Player = null;
+        
+        private EcsComponentRef<Chunk>[] _currentMap;
+
+        public void PreInit() => _currentMap = new EcsComponentRef<Chunk>[9];
+
+        public void Run()
+        {
+            /*foreach (var i in _currentMap[5].Unref().Grounds)
+            {
+                
+            }*/
+        }
+    }
+}
