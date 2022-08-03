231121aa5cfba4d0ef0a141cc6c4d049ba9f29be
Alexander <satan1c@ukr.net>
2022-08-03 16:16:32 +0300

    fix

--- Systems/General/Entities/Rufus.cs
+++ Systems/General/Entities/Rufus.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using Pathfinding;
+using Systems.General.Control;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
@@ -8,11 +9,12 @@ namespace Systems.General.Entities
 {
     public class Rufus : MonoBehaviour
     {
-        [SerializeField] private Transform Transform;
-        [SerializeField] private Transform Target;
+        [SerializeField] private Transform m_transform;
+        [SerializeField] private Transform m_target;
         [SerializeField] private AIDestinationSetter targetSetter;
 
-        [SerializeField] private Animator animator;
+        [SerializeField] private Animator m_animator;
+        [SerializeField] private SpriteRenderer m_spriteRenderer;
 
         private readonly WaitForSeconds m_timer = new(1);
         private readonly int X = Animator.StringToHash("X");
@@ -22,13 +24,28 @@ namespace Systems.General.Entities
         {
             while (true)
             {
-                var direction = new Vector3(Random.Range(-1f, 1f), Random.Range(-1f, 1f));
-                Target.position += direction;
-                animator.SetFloat(X, direction.x);
-                animator.SetFloat(Y, direction.y);
+                var direction = new Vector3(Random.Range(-1, 1) / 2f, Random.Range(-1, 1) / 2f, 0);
+                m_target.localPosition = direction;
+                var anim = ControlStick.SnapInput(direction, AxisOptions.Fixed);
+                FlipSprite(anim);
+                m_animator.SetFloat(X, anim.x);
+                m_animator.SetFloat(Y, anim.y);
 
                 yield return m_timer;
             }
         }
+
+        private void Update()
+        {
+            var pos = m_transform.position;
+            pos.z = 0;
+            m_transform.position = pos;
+        }
+        
+        private void FlipSprite(Vector2 position) 
+            => FlipSprite(position.x < -0.7f);
+
+        private void FlipSprite(bool state) 
+            => m_spriteRenderer.flipX = state;
     }
 }
